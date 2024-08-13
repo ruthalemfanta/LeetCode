fbbf2fb71aa6e26085946623d69ab25dd5d4b4e1
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-08-13 23:31:20 +0300

    back space compare

--- back_space_string_compare.py
+++ back_space_string_compare.py
@@ -1,19 +1,16 @@
 def backSpaceCompare(s, t):
-    for i in range(len(s)-1) :
-        if s[i] == "#":
-            s = s[:i-1] + s[i+1:]
-
-    for j in range(len(t)-1) :
-        if t[j] == "#":
-            t = t[:j-1] + t[j+1:]
-    if s == t:
-        return True
-    else:
-        return False
-
-
-s = "ab##"
-t = "c#d#"
+    def backSpace(string):
+        new = []
+        for char in string:
+            if char == "#" and new:
+                new.pop() 
+            elif char != "#":
+                new.append(char) 
+        return ''.join(new)  
+    
+    return backSpace(s) == backSpace(t)
 
 
-print(backSpaceCompare(s,t))
+s = "y#fo##f"
+t = "y#f#o##f"
+print(backSpaceCompare(s, t)) 
