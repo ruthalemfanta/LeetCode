ec4f07f07f0c851ad19bcfd13a593e3818f4e9fc
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-09-01 23:02:41 +0300

    min  steps

--- medium/min_steps.py
+++ medium/min_steps.py
@@ -1,11 +1,20 @@
-def minSteps(s,t):
-    s = list(s)
-    t = list(t)
-    for char in s:
-        if char in t:
-            t.remove(char)
-    return len(t)
+from collections import Counter
+def minSteps(s, t) -> int:
+    p = Counter(s)
+    q = Counter(t)
+    steps = 0
 
-s = "leetcode"
-t = "practice"
-print(minSteps(s,t))  
+    for i in p:
+        if i in q:
+            if p[i] > q[i]:
+                steps += p[i] - q[i]
+        else:
+            steps += p[i] 
+
+    return steps
+    
+        
+s = 'leetcode'
+t = 'practice'
+
+print(minSteps(s, t))
