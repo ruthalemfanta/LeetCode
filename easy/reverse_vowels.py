c4559cdea8b952e256d14d92cf89f2f7c64eeeb7
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-09-09 23:44:06 +0300

    flower bed

---
+++ easy/reverse_vowels.py
@@ -0,0 +1,14 @@
+def reverseVowels(s):
+    vowels = 'aeiouAEIOU'
+    s = list(s)
+    i, j = 0, len(s) - 1
+    while i < j:
+        if s[i] in vowels and s[j] in vowels:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        if s[i] not in vowels:
+            i += 1
+        if s[j] not in vowels:
+            j -= 1
+    return ''.join(s)
