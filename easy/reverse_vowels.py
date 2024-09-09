ef4da4e3b37528a700936326f1496832c7e1607f
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-09-09 23:53:51 +0300

    reverse vowels

--- easy/reverse_vowels.py
+++ easy/reverse_vowels.py
@@ -1,14 +1,13 @@
 def reverseVowels(s):
-    vowels = 'aeiouAEIOU'
-    s = list(s)
-    i, j = 0, len(s) - 1
-    while i < j:
-        if s[i] in vowels and s[j] in vowels:
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
-        if s[i] not in vowels:
-            i += 1
-        if s[j] not in vowels:
-            j -= 1
-    return ''.join(s)
+    order = []
+    arr = list(s)
+    for i in range(len(s)):
+        if s[i] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+            order.append(i)
+    for i in range(len(s)):
+        if s[i] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+            arr[i] = s[order.pop()]
+
+    return ''.join(arr)
+
+print(test("hello")) # "holle"
